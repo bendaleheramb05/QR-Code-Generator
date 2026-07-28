dd8517deb9487fdc85df3941bf36825d1336d312
bendaleheramb05 <bendaleheramb0@gmail.com>
2026-07-28 18:10:52 +0530

    Add files via upload

---
+++ qr_generator.py
@@ -0,0 +1,21 @@
+import qrcode
+import os
+
+
+os.makedirs("output", exist_ok=True)
+
+data = input("Enter text or URL: ")
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("output/my_qr.png")
+print("QR Code Saved Successfully")
+
